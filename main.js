82223d17ee62ca74682258a0e6152785ce5a3318
EA - Mellonny Silvestre Almeida <mellcontadeestudos@gmail.com>
2025-05-26 09:35:59 -0300

    Adição da funcionalidade de remover a classe hide-warning e também do setTimeout para adicionar a classe hide-warning

--- main.js
+++ main.js
@@ -2,4 +2,57 @@ const items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
+
+    const item = {
+        name: itemName ,
+        checked: false
+    }
+
+    items.push(item)
+
+    document.querySelector("#item").value = ""
+
+    showItemsList()
+}
+
+function showItemsList() {
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent = ""
+
+    items.map((item, index) => {
+        sectionList.innerHTML += ` 
+            <div class="item">
+                    <div>
+                        <input type="checkbox" name="list" id="item-${index}">
+
+                        <div class="custom-checkbox">
+                            <img src="./assets/checked.svg" alt="checked">
+                        </div>
+                        <label for="item-${index}">${item.name}</label>
+                    </div>
+
+
+                    <button onclick="removeItem('${item.name}')">
+                        <img src="./assets/trash-icon.svg" alt="Transh incon">
+                    </button>
+            </div>
+        `
+    })
+}
+
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+    const divWarning = document.querySelector(".warning")
+
+    divWarning.classList.remove("hide-warning")
+
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
+
+    if (itemIndex !== -1) {
+        items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
 }
